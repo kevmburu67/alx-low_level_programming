0893ef4fa4183b49dc413662b9d7e55776414ea6
Kevin Mburu <kinyuawamburu18@gmail.com>
2023-11-06 09:15:48 -0800

    preprocessor

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - prints the name of the file
  *
- * Description: Prints name of file
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-printf("%s\n", __FILE__);
-return (0);
+	printf("%s\n", __FILE__);
+	return (0);
 }
