3d710f5cd44bfdf725b24f87bcd76d4840e04029
Kevin Mburu <kinyuawamburu18@gmail.com>
2023-11-06 10:06:39 -0800

    structures

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -7,13 +7,13 @@
  */
 void free_dog(dog_t *d)
 {
-    if (d != NULL) {
-        if (d->name != NULL) {
-            free(d->name);
-        }
-        if (d->owner != NULL) {
-            free(d->owner);
-        }
-        free(d);
-    }
+	if (d != NULL) {
+		if (d->name != NULL){
+		free(d->name);
+	}
+	if (d->owner != NULL) {
+		free(d->owner);
+	}
+	free(d);
+	}
 }
